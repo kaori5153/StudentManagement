ce29eb0979f93856f09be0ffeb03369f79937db9
KaoriT <tp17.rnksk@icloud.com>
2024-10-21 23:21:39 +0900

    student_coursesテーブルの全件出力の実装

---
+++ src/main/java/raisetech/student/management/Course.java
@@ -0,0 +1,17 @@
+package raisetech.student.management;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Course {
+
+  private String courseID;
+  private String studentID;
+  private String course;
+  private String startDate;
+  private String endDate;
+
+}
--- src/main/java/raisetech/student/management/StudentRepository.java
+++ src/main/java/raisetech/student/management/StudentRepository.java
@@ -1,24 +1,15 @@
 package raisetech.student.management;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface StudentRepository {
 
-  @Select("SELECT * FROM student WHERE name = #{name}")
-  Student searchByName(String name);
+  @Select("SELECT * FROM students")
+  List<Student> searchStudent();
 
-  @Insert("INSERT student values(#{name}, #{age})")
-  void resisterStudent(String name, int age);
-
-  @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-  void updateStudent(String name, int age);
-
-  @Delete("DELETE FROM student WHERE name = #{name}")
-  void deleteStudent(String name);
-
+  @Select("SELECT * FROM students_courses")
+  List<Course> searchCourse();
 }
