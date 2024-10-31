a0205431356462f1049adb2504bb798597af9775
KaoriT <tp17.rnksk@icloud.com>
2024-10-31 23:52:03 +0900

    ServiceとContorollerの追加

---
+++ src/main/java/raisetech/student/management/service/StudentService.java
@@ -0,0 +1,27 @@
+package raisetech.student.management.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import raisetech.student.management.data.Student;
+import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.repository.StudentRepository;
+
+@Service
+public class StudentService {
+
+  private StudentRepository repository;
+
+  @Autowired
+  public StudentService(StudentRepository repository) {
+    this.repository = repository;
+  }
+
+  public List<Student> searchStudentList(){
+    return repository.searchStudent();
+  }
+
+  public List<StudentsCourses> searchStudentsCourseList(){
+    return repository.searchStudentsCourses();
+  }
+}
