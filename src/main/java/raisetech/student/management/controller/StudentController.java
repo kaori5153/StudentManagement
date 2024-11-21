6542ca1f3b5b51b1f5dbd79fd8b4f33e02fe4ed5
KaoriT <tp17.rnksk@icloud.com>
2024-11-21 23:05:46 +0900

    生徒情報とコース情報を結合するconvert機能を追加

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -5,24 +5,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.service.StudentService;
 
 @RestController
 public class StudentController {
 
   private StudentService service;
+  private StudentConverter converter;
 
   @Autowired
-  public StudentController(StudentService service) {
+  public StudentController(StudentService service, StudentConverter converter) {
     this.service = service;
+    this.converter = converter;
   }
 
   //	生徒情報を表示する
   @GetMapping("/studentList")
-  public List<Student> getStudentList(@RequestParam String age) {
-    return service.searchStudentList(age);
+  public List<StudentDetail> getStudentList(@RequestParam String age,String course) {
+    List<Student> students = service.searchStudentList(age);
+    List<StudentsCourses> studentsCourses = service.searchStudentsCourseList(course);
+    return converter.convertStudentDetails(students, studentsCourses);
   }
 
   //  受講コース情報を表示する
