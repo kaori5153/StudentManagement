f289daf14e5c6ba4422259909e6d048fdb45b2f6
KaoriT <tp17.rnksk@icloud.com>
2024-12-19 00:03:39 +0900

    講義28で行ったpost処理を追加

--- src/main/java/raisetech/student/management/controller/StudentController.java
+++ src/main/java/raisetech/student/management/controller/StudentController.java
@@ -4,11 +4,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import raisetech.student.management.controller.converter.StudentConverter;
 import raisetech.student.management.data.Student;
 import raisetech.student.management.data.StudentsCourses;
+import raisetech.student.management.domain.StudentDetail;
 import raisetech.student.management.service.StudentService;
 
 @Controller
@@ -38,6 +42,21 @@ public class StudentController {
   @GetMapping("/studentsCourseList")
   public List<StudentsCourses> getStudentsCourseList(@RequestParam String course) {
     return service.searchStudentsCourseList();
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model){
+    model.addAttribute("studentDetail", new StudentDetail());
+    return "resisterStudent";
+  }
+
+  @PostMapping("/resisterStudent")
+  public String resisterStudent(@ModelAttribute StudentDetail studentDetail, BindingResult result){
+    if(result.hasErrors()){
+      return "resisterStudent";
+    }
+    System.out.println(studentDetail.getStudent().getName() + "さんが新規受講生としてとうろくされました");
+    return "redirect:/studentList";
   }
 
 }
