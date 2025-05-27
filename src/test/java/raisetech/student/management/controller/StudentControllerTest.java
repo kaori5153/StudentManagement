9344c45dcb01f7ee6a75563fe131fc8f86a81f8c
kaori5153 <tp17.rnksk@icloud.com>
2025-05-28 00:11:47 +0900

    Controller_受講生一覧検索テストの追加

---
+++ src/test/java/raisetech/student/management/controller/StudentControllerTest.java
@@ -0,0 +1,35 @@
+package raisetech.student.management.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import raisetech.student.management.service.StudentService;
+
+@WebMvcTest(StudentControllerTest.class)
+class StudentControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private StudentService service;
+
+  @Test
+  void 受講生一覧検索が実行できリストが返ってくること() throws Exception {
+    mockMvc.perform(MockMvcRequestBuilders.get("/students"))
+        .andExpect(status().isOk());
+
+    verify(service, times(1)).searchStudentList();
+  }
+
+}
