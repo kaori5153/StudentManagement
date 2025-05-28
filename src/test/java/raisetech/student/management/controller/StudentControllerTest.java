c8e65253edf57792849231959bd3f2209a58a2c8
kaori5153 <tp17.rnksk@icloud.com>
2025-05-28 22:55:19 +0900

    Controller_受講生一覧検索テストの修正

--- src/test/java/raisetech/student/management/controller/StudentControllerTest.java
+++ src/test/java/raisetech/student/management/controller/StudentControllerTest.java
@@ -1,13 +1,13 @@
 package raisetech.student.management.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,21 +15,21 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import raisetech.student.management.service.StudentService;
 
-@WebMvcTest(StudentControllerTest.class)
+@WebMvcTest(StudentController.class)  //StudentControllerインスタンス生成される
 class StudentControllerTest {
 
   @Autowired
-  private MockMvc mockMvc;
+  private MockMvc mockMvc;  //spring bootが用意してるMockの仕組み
 
   @MockBean
   private StudentService service;
 
   @Test
-  void 受講生一覧検索が実行できリストが返ってくること() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/students"))
+  void 受講生一覧検索が実行できて空のリストが返ってくること() throws Exception {
+      mockMvc.perform(MockMvcRequestBuilders.get("/students"))//検証する内容。getの検証
         .andExpect(status().isOk());
 
-    verify(service, times(1)).searchStudentList();
+      verify(service, times(1)).searchStudentList();
   }
 
 }
